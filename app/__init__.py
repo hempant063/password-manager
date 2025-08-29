e22e889ccdb6d459f20cbc7c241ef6c3a3ca4cfd
Hans <hempant063@gmail.com>
2025-08-29 12:50:59 +0545

    completed

--- app/__init__.py
+++ app/__init__.py
@@ -11,13 +11,16 @@ def start():
 def add():
     u=input("Enter your username: ")
     p=input("Enter your password: ")
-    with open("up.txt", "w") as f:
+    with open("up.txt", "a") as f:
         f.write(f"{u}|{p}")
 
 
 def view():
-    pass
+    with open("up.txt", "r") as fo:
+        for line in fo.readlines():
+            u,p = line.split("|")
+            print(f"username: {u} and password: {p}")
 
 
-if __name__=="main":
+if __name__=="__main__":
     start()
